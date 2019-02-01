c32b175aab3dd727907e8bd01b55d1d358ea84b8
Stanislav Shapkin <shapkin359@gmail.com>
2019-02-01 23:22:56 +0300

    добавлено дз по теме Контролируемые и неконтролируемые компоненты (задания 1 и 2)

--- ra-homeworks/ctrl-component/library/js/SearchBox.js
+++ ra-homeworks/ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,24 @@
-const SearchBox = () => {
+'use strict';
+
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onChange(event) {
+    this.props.filterBooks(event.currentTarget.value);
+  }
+
+  render() {
+    let changeHandler = this.onChange.bind(this);
+
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+      <input
+        type="text"
+        placeholder="Search by title or author"
+        value={this.props.value}
+        onChange={changeHandler}
+      />
     );
-};
+  }
+}
--- ra-homeworks/ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ra-homeworks/ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,73 @@
-const SubscribeForm = () => {
+'use strict';
+
+class SubscribeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.error = 'is-error';
+    this.valid = 'is-valid';
+    this.state = {
+      valid: ''
+    };
+  }
+
+  isValid(input) {
+    if (input.value !== '') {
+      if (input.validity.valid) {
+        return this.valid;
+      } else {
+        return this.error;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  onChange() {
+    this.setState({
+      valid: this.isValid(event.currentTarget)
+    });
+  }
+
+  render() {
+    let changeHandler = this.onChange.bind(this);
+
     return (
-        <div>Поиск</div>
-    )
-};
+      <div className="subscribe__form">
+        <form className={`form form--subscribe ${this.state.valid}`}>
+          <h4 className="form-title">Подписаться:</h4>
+          <Subform
+              onChange={changeHandler}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+
+class Subform extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let changeHandler = this.props.onChange;
+
+    return (
+      <div className="form-group">
+        <label htmlFor="input-email" className="sr-only">Email</label>
+        <input
+          type="email"
+          id="input-email"
+          placeholder="Email"
+          className="form-control"
+          onChange={changeHandler}
+          />
+        <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
+        <button type="submit" className="form-next">
+          <i className="material-icons">keyboard_arrow_right</i>
+        </button>
+      </div>
+    );
+  }
+}
