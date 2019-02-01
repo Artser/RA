d0b6e24a0e5bca086aea6cc585c6cbc2251dd206
Stanislav Shapkin <shapkin359@gmail.com>
2019-02-01 23:57:35 +0300

    добавлено дз по теме Контролируемые и неконтролируемые компоненты (задание 3)

--- ra-homeworks/ctrl-component/tshirt/js/FontSelector.js
+++ ra-homeworks/ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,32 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
-    return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
+const FontSelector = ({fonts, onSelect}) => {
+
+  let changeHandler = function (event) {
+    onSelect(fonts.find(font => font.name === event.currentTarget.value));
+  }
+
+  return (
+    <div className="font-picker">
+        {fonts.map((font, index) => {
+          return (
+            <div className="grid center font-item">
+              <input
+                type="radio"
+                name="font"
+                value={font.name}
+                id={font.name}
+                onChange={changeHandler}
+              />
+              <label htmlFor={font.name} className="grid-1">
+                <PictureFont
+                  text={font.name.slice(0, 3)}
+                  path={font.path}
+                  key={index}
+                />
+              </label>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 };
--- ra-homeworks/ctrl-component/tshirt/js/TextRenderLine.js
+++ ra-homeworks/ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,20 @@
 const TextRenderLine = ({value, onChange}) => {
+	let changeHandler = function (event) {
+		onChange(event.currentTarget.value.replace(/[^A-Za-z\s]/g, ''));
+	}
+
 	return (
 		<div className="type-text">
-			Наберите текст
+			<textarea 
+				name="text" 
+				id="font-text" 
+				cols="30" 
+				rows="2" 
+				placeholder="Введите текст для футболки"
+				value={value}
+				onChange={changeHandler}
+				>
+			</textarea>
 		</div>
 	);
 };
